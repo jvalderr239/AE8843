44117e924379aa54f865ed6fcbb67bd0ec6a220c
josev <jvalderr@gatech.edu>
2020-11-15 04:15:24 -0500

    1. Updated scipy library 2. Modified functions: propagate, sys_func

--- MPC_Controller.py
+++ MPC_Controller.py
@@ -1,19 +1,20 @@
 import numpy as np
 from control import matlab as mat
 from scipy import integrate
+from scipy.linalg import solve_continuous_are
 
 
 class MPC:
     x = 0
     y = 1
     theta = 2
-    dT = .1
+    dT = .5
     v_max = 0.6
     v_min = -v_max
     omega_max = np.pi/4
     omega_min = -omega_max
     wheel_base = .7
-    t0, tf = 0, 20
+    t0, tf = 0, 5
     time = np.linspace(t0, tf, int(1/dT))
 
     def __init__(self, start_state, goal_state, start_control, N, Q, R):
@@ -30,7 +31,7 @@ class MPC:
         self.A = np.eye(self.num_states)
         self.B = np.array([[np.cos(start_state[-1]), 0],
                      [np.sin(start_state[-1]), 0],
-                     [0, 1]])
+                     [0, 1]], dtype=np.float)
         self.Q = Q
         self.R = R
 
@@ -40,6 +41,7 @@ class MPC:
         self.state[:, [0]] = temp
 
         for idx in range(1, self.horizon):
+            print("Current initial state: \n")
             print(temp, idx)
 
             self.state[:, [idx]] = self.propagate(temp, idx)
@@ -48,13 +50,17 @@ class MPC:
     def propagate(self, vector, idx):
 
         theta = vector[-1]
+
         b = np.array([[np.cos(theta), 0],
                      [np.sin(theta), 0],
-                     [0, 1]])
-        P = mat.care(self.A, b, self.Q, self.R)
-        K = np.linalg.inv(self.R)*b.T*P
-        self.controls[:, [idx]] = integrate.solve_ivp(self.sys_func, self.time, vector, args=(self.A, b, K))
-        print(self.controls[:, [idx]])
+                     [0, 1]], dtype=np.float)
+        P = solve_continuous_are(self.A, b, self.Q, self.R)
+        K = np.linalg.inv(self.R).dot(np.dot(b.T, P))
+
+        sol = integrate.solve_ivp(
+            self.sys_func, self.time, vector.T[0], args=(self.A, b, K), method='RK45', t_eval=self.time)
+
+        self.controls[:, [idx]] = np.matmul(-K, sol.y[:, [1]])
         return self.A.dot(vector) + self.dT*b.dot(self.controls[:, [idx]])
 
     def compute_objective_func(self):
@@ -67,29 +73,34 @@ class MPC:
     def compute_constraints(self):
         pass
 
-    @staticmethod
-    def sys_func(t, x, A, b, K): return (A - b*K)*x
+    def sys_func(self, t, x, a, b, k):
+        print("X: ", "\t", str(x))
+        print("A", "\t", a, a.shape, "\t", t)
+        print("B", "\t", b, b.shape, "\t", t)
+        print("K", "\t", k, k.shape, "\t", t)
+        print("------current solution--------")
+        return np.matmul(a - b.dot(k), x.reshape(self.num_states, 1)).T
 
 
 if __name__ == '__main__':
 
     # Define gains
-    kx = 1
-    ky = 5
-    ktheta = .2
-    kv = .5
-    komega = .05
+    kx = 15
+    ky = 10
+    ktheta = 1
+    kv = 100
+    komega = 200
 
     # Choose horizon
     horizon = 5
-    start = np.array([[0], [0], [0]])
-    goal = np.array([[2], [2], [0]])
-    start_control = np.zeros((2, 1))
+    start = np.array([[0], [0], [np.pi/10]], dtype=np.float)
+    goal = np.array([[2], [2], [0]], dtype=np.float)
+    start_control = np.full((2, 1), .06, dtype=np.float)
 
     Q = np.array([[kx, 0, 0],
                   [0, ky, 0],
-                  [0, 0, ktheta]])
-    R = np.array([[kv, 0], [0, komega]])
+                  [0, 0, ktheta]], dtype=np.float)
+    R = np.array([[kv, 0], [0, komega]], dtype=np.float)
 
     counter = 0
     solution = []
