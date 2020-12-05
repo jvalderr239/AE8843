882e08a3e34caad9a6c0390faf88aef6a97a7c3b
Jose Valderrama <49346073+jvalderr239@users.noreply.github.com>
2020-12-05 14:11:18 -0500

    Update dMPC_controller.m
    Updated minimization function to minimize deviation from goal state
    Changed input to row vector

--- dMPC_controller.m
+++ dMPC_controller.m
@@ -1,11 +1,12 @@
 %% dMPC Script
 clc;clear;close all;
 
+rng('default')
 % Initial State [x;y;theta]
 variance = 0.0000001;
 
-init_state = [-8;
-              -9;
+init_state = [20;
+              20;
               0];
 y_hat_nl(:,1) = init_state;         
 y_hat = init_state(3);
@@ -14,15 +15,21 @@ goal_state = [8;
               9;
               0];
           
-init_cntrl = [0;0];         
+init_cntrl = [0,0];         
 
 % System Mat
 
-sys_mat = [1,0,0;
+A = [1,0,0;
            0,1,0;
            0,0,1]; % A mat
        
-A = sys_mat;
+sys_mat = A;
+
+% Try to make an estimate of sys mat
+param_est = randn(1,3);
+
+sys_mat_est = diag(param_est);
+
 
 % Control Variables[v;w]
        
@@ -30,7 +37,7 @@ A = sys_mat;
 %                sin(theta), 0;
 %                0,          1]; % B Mat
 
-Tf = 1; % seconds
+Tf = 5; % seconds
 dt = 0.01;
 time_series = 0:dt:Tf;
 
@@ -40,19 +47,22 @@ r = 0.5;
 alpha = 0.25;
 theta_hat = [0;0;0];
 horizon = 3; % N = 3   
-P_mat = eye(3);
+P_mat = 1000*eye(3);
 
 xstar(:,1) = init_state;
-ustar(:,1) = init_cntrl;
+ustar(:,1) = init_cntrl';
 for i = 2:length(time_series)
-    xt = xstar(:,i-1); 
-    ut = xstar(:,i-1); 
+    %Updated - JEV
+    % ut should use ustar input
+    % xt can be deviation so fmincon will drive to zero dev
+    %
+    xt = xstar(:,i-1)-goal_state; 
+    ut = ustar(:,i-1); 
     theta = xt(3);
     v = ut(1);
     B = [cos(theta), v*sin(theta);
          sin(theta), -v*cos(theta); 
          0,          0]; %Linearized control_mat w.r.t theta, v, and w                      
-
     [k_star,~,~] = icare(A,B,theta_hat*theta_hat');
     %% Minimize: y_hakt^2 + r*ustar^2 + xt'*P_mat*xt... to get ustar 
     %% Need to update with Unicycle expansion and both control inputs
@@ -65,22 +75,29 @@ for i = 2:length(time_series)
     min_func = @(ustar_min)((y_hat'*y_hat + r*[ustar_min(1);ustar_min(2)]'*[ustar_min(1);ustar_min(2)] + xt'*P_mat*xt + ...
                 alpha*(theta_hat'*(xt+(A*xt+B*[ustar_min(1);ustar_min(2)])*dt) + r*[ustar_min(1);ustar_min(2)]'*[ustar_min(1);ustar_min(2)] + (xt+(A*xt+B*[ustar_min(1);ustar_min(2)])*dt)'*(P_mat + (xt+(A*xt+B*[ustar_min(1);ustar_min(2)])*dt)*(xt+(A*xt+B*[ustar_min(1);ustar_min(2)])*dt)')^-1*(xt+(A*xt+B*[ustar_min(1);ustar_min(2)])*dt)) + ...
                 (alpha*alpha)*(theta_hat'*(xt+(A*(xt+(A*xt+B*[ustar_min(1);ustar_min(2)])*dt)+ B*[ustar_min(1);ustar_min(2)])*dt) + r*[ustar_min(1);ustar_min(2)]'*[ustar_min(1);ustar_min(2)] + (xt+(A*(xt+(A*xt+B*[ustar_min(1);ustar_min(2)])*dt)+ B*[ustar_min(1);ustar_min(2)])*dt)'*((P_mat + (xt+(A*xt+B*[ustar_min(1);ustar_min(2)])*dt)*(xt+(A*xt+B*[ustar_min(1);ustar_min(2)])*dt)'+(xt+(A*(xt+(A*xt+B*[ustar_min(1);ustar_min(2)])*dt)+ B*[ustar_min(1);ustar_min(2)])*dt)*(xt+(A*(xt+(A*xt+B*[ustar_min(1);ustar_min(2)])*dt)+ B*[ustar_min(1);ustar_min(2)])*dt)'))^-1*(xt+(A*(xt+(A*xt+B*[ustar_min(1);ustar_min(2)])*dt)+ B*[ustar_min(1);ustar_min(2)])*dt))));
-
+    
     %         x = fmincon(@(ustar)y_hat*y_hat + r*ustar*ustar + xt'*P_mat*xt,[0],[],[],[],[],[-1],[1]);
-    x = fmincon(min_func,[0.5;-0.5],[],[],[],[],[-1;-1],[1;1]);
+    
+    %Updated - JEV
+    %Returns optimal input as row vector so needs input as row vector
+    %
+    linear_uopt = fmincon(min_func,init_cntrl ,[],[],[],[],[-1;-1],[1;1]);
     %%
-    ustar(:,i-1) = x;
+    ustar(:,i) = linear_uopt';
+    init_cntrl = ustar(:,i)';
 %     if (i > 3) && (sign(xstar(2,i-2)) ~= sign(xstar(2,i-1)))
 %         ustar(i-1) = 0;
 %     end
-
+    
     control_mat = [cos(theta), 0;
                    sin(theta), 0;
                    0,          1]; % B Mat
-
-    y_hat_nl(:,i) = y_hat_nl(:,i-1)+(sys_mat * y_hat_nl(:,i-1) + control_mat * ustar(:,i-1))*dt + (sqrt(variance) * randn()); % Nonlinear Model Update
+    
+    y_hat_nl(:,i) = y_hat_nl(:,i-1)+...
+        (sys_mat * y_hat_nl(:,i-1) + control_mat * ustar(:,i-1))*dt + ...
+        (sqrt(variance) * randn()); % Nonlinear Model Update
     y_hat = y_hat_nl(3);
-    xstar(:,i) = xt+(A*xt+B*ustar(:,i-1))*dt;   %Linearized Model Update
+    xstar(:,i) = (A*xt+B*ustar(:,i))*dt;   %Linearized Model Update
     xt = xstar(:,i);
     % After finding min and propagating, recalculate G, theta_hat, and
     % P
@@ -89,24 +106,29 @@ for i = 2:length(time_series)
     P_mat = (eye(3) - G*xt')*P_mat;
 end
 
+% Switch from deviation space to state space
+iters = size(xstar,2);
+ optimal_x(:, 1) = init_state;
+for idx = 2:iters
+    optimal_x(:, idx) = xstar(:,idx)+ goal_state;
+end
+
 figure
 hold on
-plot(xstar(1,:),xstar(2,:))
+plot(optimal_x(1,:), optimal_x(2,:))
 plot(y_hat_nl(1,:),y_hat_nl(2,:),'r')
-legend('Linearized','Nonlinear Observation')
+legend('Linearized', 'Non-Linear')
 title('x-y plot')
 
 figure
 hold on
-plot(time_series(2:end),ustar(1,:))
+plot(time_series(1:end),ustar(1,:))
 title('v control plot')
 
 figure
 hold on
-plot(time_series(2:end),ustar(2,:))
+plot(time_series(1:end),ustar(2,:))
 title('w control plot')
-
-
 
 
 
