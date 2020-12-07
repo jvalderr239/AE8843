57d524e15733394eefa302d2e9807b424436ec5f
Aayush Sharma <aayush.sharma374@gatech.edu>
2020-12-06 22:30:09 -0500

    Fixing x propagation

--- dMPC_recreate.m
+++ dMPC_recreate.m
@@ -38,7 +38,7 @@ B = [1;
 % measurement equations.
 theta_truth = [5;
                2;
-               -1;]
+               -1];
            
 Tf = 5; % seconds
 dt = 0.01;
@@ -62,8 +62,6 @@ for i = 2:length(time_series)
     %
     xt = xstar(:,i-1)-goal_state; 
     ut = ustar(:,i-1); 
-    theta = xt(3);
-    v = ut(1);
     
     %Update - SRR:
     %i believe this is DARE, not CARE. using idare now intead of icare.
@@ -88,14 +86,6 @@ for i = 2:length(time_series)
     % SRR concern - paper says transpose(phi)*z >= 0 . How are we enforcing
     % this below? If problems persist, TODO:  Verify this actually holds true 
      
-%     min_func = @(ustar_min)(-(y_hat'*y_hat + r*[ustar_min(1);ustar_min(2)]'*[ustar_min(1);ustar_min(2)] + xt'*P_mat*xt + ...
-%                 alpha*(theta_hat'*(xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt) + r*[ustar_min(1);ustar_min(2)]'*[ustar_min(1);ustar_min(2)] + (xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt)'*(P_mat + (xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt)*(xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt)')^-1*(xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt)) + ...
-%                 (alpha*alpha)*(theta_hat'*(xt+(A*(xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt)+ B*[ustar_min(1);ustar_min(2)])*dt) + r*[ustar_min(1);ustar_min(2)]'*[ustar_min(1);ustar_min(2)] + (xt+(A*(xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt)+ B*[ustar_min(1);ustar_min(2)])*dt)'*((P_mat + (xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt)*(xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt)'+(xt+(A*(xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt)+ B*[ustar_min(1);ustar_min(2)])*dt)*(xt+(A*(xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt)+ B*[ustar_min(1);ustar_min(2)])*dt)'))^-1*(xt+(A*(xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt)+ B*[ustar_min(1);ustar_min(2)])*dt)) + ...
-%                   (alpha*alpha*alpha)*(xt+(A*(xt+(A*(xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt)+ B*[ustar_min(1);ustar_min(2)])*dt) + B*[ustar_min(1);ustar_min(2)])*dt)'*(k_star)*(xt+(A*(xt+(A*(xt+(A*xt+B.*[ustar_min(1);ustar_min(2)])*dt)+ B*[ustar_min(1);ustar_min(2)])*dt) + B*[ustar_min(1);ustar_min(2)])*dt)));
-
-%     min_func = @(ustar_min)((y_hat'*y_hat + r*ustar_min'*ustar_min + xt'*P_mat*xt));
-    %         x = fmincon(@(ustar)y_hat*y_hat + r*ustar*ustar + xt'*P_mat*xt,[0],[],[],[],[],[-1],[1]);
-    
     % AS Defining H, Aeq, Aineq, beq, and bineq matrices and vectors for
     % quadprog
     Hmat = zeros(17);
@@ -110,14 +100,6 @@ for i = 2:length(time_series)
     Hmat(5,3) = k_star(3,1);
     Hmat(5,4) = k_star(3,2);
     Hmat(5,5) = k_star(3,3);
-
-%     Hmat = 0.5*Hmat;
-            
-%     Hmat = 0.5*[1, 0, 0, 0, 0;         % y_hat       
-%                 0, r, 0, 0, 0;
-%                 0, 0, k_star, 0, 0;
-%                 0, 0, 0, 0, 0;
-%                 0, 0, 0, 0, 0];
 
     % UPDATE AS : Updated the f_vec 
     f_vec =  zeros(17,1);
@@ -136,12 +118,6 @@ for i = 2:length(time_series)
     
     Aeq(5,2) = -B(3,1);
     Aeq(5,5) = 1;
-
-%     Aeq(6,2) = ;
-%     
-%     Aeq(7,2) = ;
-%     
-%     Aeq(8,2) = ;
     
     Aeq(6,6) = R_mat(1,1);
     Aeq(6,7) = R_mat(1,2);
@@ -165,12 +141,6 @@ for i = 2:length(time_series)
     Aeq(15,15) = 1;
     Aeq(16,16) = 1;
     Aeq(17,17) = 1;
-%     
-%     Aeq = [1, 0, 0, 0, 0;
-%            0, 0, 0, 0, 0;
-%            0, -B,1, 0, 0;
-%            0, 0, 0, P_mat^-1, 0;
-%            0, 0, 0, 0, 1];
 
     beq = zeros(17,1);
     beq(1) = y_hat;
@@ -190,18 +160,12 @@ for i = 2:length(time_series)
     beq(15) = R_mat(3,1);
     beq(16) = R_mat(3,2);
     beq(17) = R_mat(3,3);
-%     beq = [y_hat; 0; A*xt; xt; P_mat^-10];
     
     Aineq = zeros(17);
     Aineq(6,6) = -xt(1,1);
     Aineq(7,7) = -xt(2,1);
     Aineq(8,8) = -xt(3,1);
     
-%     Aineq = [0, 0, 0, 0,    0;
-%              0, 0, 0, 0,    0;
-%              0, 0, 0, 0,    0;
-%              0, 0, 0, -xt', 0;
-%              0, 0, 0, 0,    0];
     bineq = zeros(17,1);
     
     lb = ones(17,1)*-inf;
@@ -210,42 +174,35 @@ for i = 2:length(time_series)
     
     ub = lb*-1;
     %% quadprog analysis
-%     linear_uopt = quadprog(0.5*Hmat,f_vec,Aineq,bineq,Aeq,beq,lb,ub);
+    linear_uopt = quadprog(0.5*Hmat,f_vec,Aineq,bineq,Aeq,beq,lb,ub);
 
     %% Gurobi Definition
-    model.obj = f_vec;
-    model.Q = sparse(Hmat);
-    model.A = sparse([Aeq;Aineq]);
-    model.rhs = [beq;bineq];
-    
-    ne = size(Aeq,1);
-    ni = size(Aineq,1);
-    model.sense = [repmat('=',ne,1);repmat('<',ni,1)];
-    model.lb = lb;
-    model.ub = ub;
-
-    params.outputflag = 0;
-%     params.NonConvex = 2;
-    % params.DualReductions = 0;
-    result = gurobi(model, params);
-    disp(result.status)
-    linear_uopt = result.x;
-    %Updated - JEV
-    %Returns optimal input as row vector so needs input as row vector
-    %
-%     linear_uopt = fmincon(min_func,init_cntrl ,[],[],[],[],[-100],[100]);
+%     model.obj = f_vec;
+%     model.Q = sparse(Hmat);
+%     model.A = sparse([Aeq;Aineq]);
+%     model.rhs = [beq;bineq];
+%     
+%     ne = size(Aeq,1);
+%     ni = size(Aineq,1);
+%     model.sense = [repmat('=',ne,1);repmat('<',ni,1)];
+%     model.lb = lb;
+%     model.ub = ub;
+% 
+%     params.outputflag = 0;
+% %     params.NonConvex = 2;
+%     % params.DualReductions = 0;
+%     result = gurobi(model, params);
+%     disp(result.status)
+%     linear_uopt = result.x;
     %%
     ustar(:,i) = linear_uopt(2);
-%     init_cntrl = ustar(:,i)';
-%     if (i > 3) && (sign(xstar(2,i-2)) ~= sign(xstar(2,i-1)))
-%         ustar(i-1) = 0;
-%     end
     
     % UPDATE SRR - y_hat_nl is the "output measurement", from page 1 
     % system(1) in the  paper. reworked this equation to match the eqns.
     y_hat_nl(:,i) = theta_truth'*xt + (sqrt(variance) * randn(1,1)); 
     y_hat = y_hat_nl(:,i);
-    xstar(:,i) = xt + (A*xt+B*ustar(:,i))*dt;   %Linearized Model Update
+%   AS Fixing propagation  
+    xstar(:,i) = (A*xt+B*ustar(:,i));   %Linearized Model Update
     xt = xstar(:,i);
     % After finding min and propagating, recalculate G, theta_hat, and
     % P
